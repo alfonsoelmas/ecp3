7f953460bcd59965e0fa13930ac4d14cb2d6f72c
Experimentando y aprendiendo <alfonso_soria_m@hotmail.com>
2017-04-03 20:35:22 +0200

    Cambios
    Añadidos cambios

---
+++ pr3/2017/a/D8Led.h
@@ -0,0 +1,13 @@
+/*-------------------------------------------------------------------
+				Alfonso Soria Muñoz - Adrián
+*-----------------------------------------------------------------*/
+
+#ifndef D8LED_H_
+#define D8LED_H_
+
+void D8Led_init(void);
+void D8Led_segment(int value1, int value2);
+void D8Led_digit(int value); 
+
+#endif
+
---
+++ pr3/2017/a/intcontroller.c
@@ -0,0 +1,116 @@
+/*-------------------------------------------------------------------
+
+		Alfonso Soria Muñoz - Adrián
+
+-----------------------------------------------------------------*/
+
+
+#include "44b.h"
+#include "intcontroller.h"
+
+void ic_init(void)
+{
+
+	rINTMOD = 0x0; 
+	rINTCON = 0x7; 
+	rINTMSK = ~(0x0); 
+
+int ic_conf_irq(enum enable st, enum int_vec vec)
+{
+	int conf = rINTCON;
+
+	if (st != ENABLE && st != DISABLE){
+		return -1;
+	}
+
+	if (vec == VEC){		
+		conf &= ~(0x1 << 2);
+	}
+	else	
+		conf |= (0x1 << 2);
+
+	if (st == ENABLE)
+		conf &= ~(0x1 << 1);
+	else
+		conf |= (0x1 << 1);
+
+	rINTCON = conf;
+	return 0;
+}
+
+int ic_conf_fiq(enum enable st)
+{
+	int ret = 0;
+
+	if (st == ENABLE)
+		
+		rINTCON &= ~(0x1);
+	else if (st == DISABLE)
+		
+		rINTCON |= (0x1);
+	else
+		ret = -1;
+
+	return ret;
+}
+
+int ic_conf_line(enum int_line line, enum int_mode mode)
+{
+	unsigned int bit = INT_BIT(line);
+
+	if (line < 0 || line > 26)
+		return -1;
+
+	if (mode != IRQ && mode != FIQ)
+		return -1;
+
+	if (mode == IRQ){		
+		rINTMOD &= ~bit;
+	}
+	else		
+		rINTMOD |= bit;
+
+	return 0;
+}
+
+int ic_enable(enum int_line line)
+{
+	if (line < 0 || line > 26)
+		return -1;
+
+	
+	rINTMSK &= ~((0x1 << line) | (0x1 << INT_GLOBAL));
+
+	return 0;
+}
+
+int ic_disable(enum int_line line)
+{
+	if (line < 0 || line > 26)
+		return -1;
+
+	rINTMSK |= (0x1 << line);
+
+	return 0;
+}
+
+int ic_cleanflag(enum int_line line)
+{
+	int bit;
+
+	if (line < 0 || line > 26)
+		return -1;
+
+	bit = INT_BIT(line);
+
+	if (rINTMOD & bit){
+		rF_ISPC = bit;
+	}
+	else
+		rI_ISPC = bit;
+
+	return 0;
+}
+
+
+
